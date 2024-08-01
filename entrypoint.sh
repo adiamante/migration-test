4f2626bd55240f760f3766d7152d5ab43dae905a
angelo.diamante <angelo.diamante@core10.io>
2024-08-01 11:11:36 -0500

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo "Starting pg_prove"
 export PGPASSWORD=$PGTAP_PASSWORD
 
 # https://github.com/theory/pgtap/issues/190
-psql -U $PGTAP_USER -d $PGTAP_DB -c "CREATE EXTENSION IF NOT EXISTS pgtap;"
+#psql -U $PGTAP_USER -d $PGTAP_DB -c "CREATE EXTENSION IF NOT EXISTS pgtap;"
 SQL_FILES=$(find /tests -type f -name "*.sql")
 pg_prove -h postgres -U $PGTAP_USER -d $PGTAP_DB -s $PG_SCHEMA $SQL_FILES
 STATUS_CODE=$?
