8430a5dd6461a8bd7fae88da3d3f3a33b16710aa
angelo.diamante <angelo.diamante@core10.io>
2024-08-01 11:43:43 -0500

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,9 @@ export PGPASSWORD=$PGTAP_PASSWORD
 # https://github.com/theory/pgtap/issues/190
 SQL_FILES=$(find /tests -type f -name "*.sql")
 echo $SQL_FILES
-pg_prove -h postgres -U $PGTAP_USER -d $PGTAP_DB -s $PG_SCHEMA $SQL_FILES
+#pg_prove -h postgres -U $PGTAP_USER -d $PGTAP_DB -s $PG_SCHEMA $SQL_FILES
+pg_prove -h postgres -U $PGTAP_USER -d $PGTAP_DB -s $PG_SCHEMA --ext .sql --recurse tests/
+
 STATUS_CODE=$?
 echo "pg_prove completed"
 exit $STATUS_CODE
