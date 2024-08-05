9c6fd1d5c5e0b3512b4a638e8f080e09e7b1224b
angelo.diamante <angelo.diamante@core10.io>
2024-08-05 09:43:15 -0500

    update test

--- tests/schema/test_schema.sql
+++ tests/schema/test_schema.sql
@@ -6,10 +6,12 @@ SELECT plan(4);
 --SELECT plan(99999); --test unit test fail
 
 -- Test if the table 'customers' exists
-SELECT has_table('dbo'::name, 'customers'::name);
+--SELECT has_table('dbo'::name, 'customers'::name);
+SELECT has_table('customers'::name);
 
 -- Test if the primary key 'customer_pkey' exists on 'customers'
-SELECT has_pk( 'dbo', 'customers'::name );
+--SELECT has_pk( 'dbo', 'customers'::name );
+SELECT has_pk('customers'::name );
 
 -- Test if the function 'calculate_sum' exists in 'dbo' schema
 SELECT has_function('calculate_sum');
